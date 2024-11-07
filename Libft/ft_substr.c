100ca859d5d67a585710877225633b4609a94990
CyberOneFR <erwoan.thebault@gmail.com>
2024-11-07 18:06:51 +0100

    this is an autmatic push

--- Libft/ft_substr.c
+++ Libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: ethebaul <ethebaul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 15:25:03 by ethebaul          #+#    #+#             */
-/*   Updated: 2024/11/07 17:43:06 by ethebaul         ###   ########.fr       */
+/*   Updated: 2024/11/07 18:06:10 by ethebaul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,14 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	i;
 
 	i = 0;
-	sub = (char *)malloc(len * sizeof(char));
+	while (i < len && s[start + i])
+		i++;
+	sub = (char *)malloc(i * sizeof(char));
 	if (!sub)
 		return ((char *)0);
 	while (i < len)
 	{
-		sub[i] = ((char *)s + start)[i];
+		sub[i] = s[start + i];
 		i++;
 	}
 	return (sub);
